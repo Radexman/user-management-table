f7933c26a7b7d909a39ca0a0b3cf963ce89c6e82
Radexman <radoslaw.siek-web.dev@wp.pl>
2024-09-10 14:49:39 +0200

    Correct URL

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -7,48 +7,48 @@ const url = import.meta.env.VITE_API_URL;
 
 // Define a type for the slice state
 interface UserState {
-	users: User[];
-	status: 'idle' | 'loading' | 'succeeded' | 'failed';
-	error: string | null;
+  users: User[];
+  status: 'idle' | 'loading' | 'succeeded' | 'failed';
+  error: string | null;
 }
 
 // Define the initial state using that type
 const initialState: UserState = {
-	users: [],
-	status: 'idle',
-	error: null,
+  users: [],
+  status: 'idle',
+  error: null,
 };
 
 // Async thunk to fetch users
 export const fetchUsers = createAsyncThunk<User[]>('users/fetchUsers', async () => {
-	try {
-		const response = await axios.get(url);
-		// Validate response data using Zod
-		const validateUsers = usersSchema.parse(response.data);
-		return validateUsers;
-	} catch (error) {
-		throw error;
-	}
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    // Validate response data using Zod
+    const validateUsers = usersSchema.parse(response.data);
+    return validateUsers;
+  } catch (error) {
+    throw error;
+  }
 });
 
 export const userSlice = createSlice({
-	name: 'users',
-	initialState,
-	reducers: {},
-	extraReducers: (builder) => {
-		builder
-			.addCase(fetchUsers.pending, (state) => {
-				state.status = 'loading';
-			})
-			.addCase(fetchUsers.fulfilled, (state, action: PayloadAction<User[]>) => {
-				state.status = 'succeeded';
-				state.users = action.payload;
-			})
-			.addCase(fetchUsers.rejected, (state, action) => {
-				state.status = 'failed';
-				state.error = action.error.message || 'Failed to fetch users';
-			});
-	},
+  name: 'users',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchUsers.fulfilled, (state, action: PayloadAction<User[]>) => {
+        state.status = 'succeeded';
+        state.users = action.payload;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message || 'Failed to fetch users';
+      });
+  },
 });
 
 export const selectUsers = (state: RootState) => state.users.users;
